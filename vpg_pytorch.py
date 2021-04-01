8f6a9ae990b8395727c7789b0898648c8a8dd48a
daek <dk.zz79ya@gmail.com>
2021-04-01 15:55:01 +0900

    Save, load checkpoint for every 50 th episode
    - Enable checkpoint-saving for every 50 th episode
    - Enable gin-config
    
    If you want to restart training from a checkpoint, modify the
    `train.cp_path` attribute in config.gin as the checkpoint's path.

--- vpg_pytorch.py
+++ vpg_pytorch.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 import numpy as np
 import gym
-from gym.spaces import Discrete, Box
 import torch.nn as nn
 import torch
 import torch.optim as optimizer
@@ -8,6 +8,8 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pathlib import Path
+import logging
+import gin.torch
 
 
 class MLP(nn.Module):
@@ -22,30 +24,51 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         layers = []
         for i in range(len(sizes) - 1):
-            layers.append(nn.Linear(sizes[i], sizes[i+1]))
+            layers.append(nn.Linear(sizes[i], sizes[i + 1]))
             if i < len(sizes) - 2:
                 layers.append(activation)
         self.fwd = nn.Sequential(*layers)
-    
+
     def forward(self, x):
         return F.softmax(self.fwd(x), dim=-1)
 
-def discount_rewards(rewards, gamma=0.99):
+
+def discount_rewards(reward, gamma=0.99):
     # Cumulative discounted sum
-    r = np.array([gamma**i * rewards[i] 
-                  for i in range(len(rewards))])
+    r = np.array([gamma ** i * reward[i]
+                  for i in range(len(reward))])
     r = r[::-1].cumsum()[::-1]
-    # Subtracting the baseline reward 
+    # Subtracting the baseline reward
     # Intuitively this means if the network predicts what it
     # expects it should not do too much about it
-    # Stabalizes and speeds up the training 
+    # Stabalizes and speeds up the training
     return r - r.mean()
 
-def train(env_name='CartPole-v0', start_time_str=None, num_episodes=2000, batch_size=10, lr=1e-2, gamma=0.99):
-    if start_time_str is None:
-        start_time_str = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
-    env = gym.make(env_name)
+
+@gin.configurable
+def train(env_id='CartPole-v0', save_dir=None, num_episodes=2000, batch_size=10, lr=1e-2, gamma=0.99, cp_path=None):
+    if save_dir is None:
+        save_dir = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+        Path(f'Save/{save_dir}').mkdir(parents=True, exist_ok=True)
+    env = gym.make(env_id)
+    op = getattr(env, "getLogger", None)
+    if op is not None:
+        logger = env.getLogger()
+    else:
+        logger = logging.getLogger('vpg')
+
     model = MLP([env.observation_space.shape[0], 32, 20, env.action_space.n])
+    opt = optimizer.Adam(model.parameters(), lr)
+    prev_episode = 0
+    if cp_path is not None:
+        checkpoint = torch.load(cp_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        opt.load_state_dict(checkpoint['optimizer_state_dict'])
+        prev_episode = checkpoint['episode']
+        logger.info(f"Loaded successfully checkpoint from file {cp_path}")
+        logger.info(f"Restarting from {prev_episode} th epsiode...")
+
+    action_space = np.arange(env.action_space.n)
 
     # Lists for maintaing logs
     total_rewards = []
@@ -54,38 +77,36 @@ def train(env_name='CartPole-v0', start_time_str=None, num_episodes=2000, batch_
     batch_states = []
 
     batch_counter = 0
-    opt = optimizer.Adam(model.parameters(), lr)
-    action_space = np.arange(env.action_space.n)
 
-    for ep in range(num_episodes):
+    for ep in range(prev_episode, num_episodes):
         # Reset
         s_0 = env.reset()
         states = []
-        rewards = []
+        reward = []
         actions = []
         complete = False
-        while complete == False:
+        while not complete:
             action_probs = model(torch.FloatTensor(s_0)).detach().numpy()
             action = np.random.choice(action_space, p=action_probs)
             s1, r, complete, _ = env.step(action)
             states.append(s_0)
-            rewards.append(r)
+            reward.append(r)
             actions.append(action)
             s_0 = s1
             if complete:
-                batch_rewards.extend(discount_rewards(rewards, gamma))
+                logger.info(f"Ep {ep + 1}, step_cnt = {len(states)}")
+                batch_rewards.extend(discount_rewards(reward, gamma))
                 batch_states.extend(states)
                 batch_actions.extend(actions)
                 batch_counter += 1
-                total_rewards.append(sum(rewards))
+                total_rewards.append(sum(reward))
 
                 if batch_counter == batch_size:
-                    print("Ep: {} Average of last 100: {:.2f}.".format(
-                        ep + 1, np.mean(total_rewards[-100:])))        
-                    # Save the last trained model's weights
-                    if len(total_rewards) >= 100 and np.mean(total_rewards[-100:]) >= 195:
-                        filename = 'Save/%s/vpg_weight_ep%d.pth' % (start_time_str, ep + 1)
-                        print("Save weight for evaluating... path = %s" % (filename))
+                    logger.info(f"To update. Last 100 ep. avg. reward = {np.mean(total_rewards[-100:])}.")
+                    # Save the last trained model's weights if they are good
+                    if len(total_rewards) >= 100 and np.mean(total_rewards[-100:]) >= 20000:
+                        filename = 'Save/%s/vpg_weight_ep%d.pth' % (save_dir, ep + 1)
+                        logger.info(f"Save weight for eval: path = {filename}")
                         torch.save(
                             model.state_dict(),
                             filename
@@ -104,7 +125,7 @@ def train(env_name='CartPole-v0', start_time_str=None, num_episodes=2000, batch_
                     # Loss is negative of expected policy function J = R * log_prob
                     loss = -selected_log_probs.mean()
 
-                    # Do the update gradient descent(with negative reward hence is gradient ascent) 
+                    # Do the update gradient descent(with negative reward hence is gradient ascent)
                     loss.backward()
                     opt.step()
 
@@ -114,26 +135,32 @@ def train(env_name='CartPole-v0', start_time_str=None, num_episodes=2000, batch_
                     batch_counter = 0
 
                     # Save the current learning info as a checkpoint for every 1000 Ep.
-                    if ep % 1000 == 999:
+                    if ep % 50 == 49:
+                        path = f'Save/{save_dir}/vpg_cp_ep_{ep + 1}.pth'
                         torch.save({
-                            'episode': ep,
+                            'episode': ep + 1,
                             'model_state_dict': model.state_dict(),
                             'optimizer_state_dict': opt.state_dict(),
                             'loss': loss
-                        }, 'Save/%s/vpg_cp_ep_%d.pth' % (start_time_str, ep + 1))        
-    
+                        }, path)
+                        logger.info(f"Checkpoint saved to file {path}")
+
     return total_rewards
+
 
 if __name__ == '__main__':
     start_time_str = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
     Path('Save/%s' % start_time_str).mkdir(parents=True, exist_ok=True)
-    rewards = train(start_time_str=start_time_str, num_episodes=10000)
+    gin.parse_config_file('config.gin')
+    rew = train()
+    # env_name = 'gym_rds_cartpole:RdsCartPole-async-v0'
+    # rew = train(env_id=env_name, save_dir=start_time_str, num_episodes=10000)
     window = 10
-    smoothed_rewards = [np.mean(rewards[i-window:i+1]) if i > window 
-                        else np.mean(rewards[:i+1]) for i in range(len(rewards))]
+    smoothed_rewards = [np.mean(rew[i - window:i + 1]) if i > window
+                        else np.mean(rew[:i + 1]) for i in range(len(rew))]
 
-    plt.figure(figsize=(12,8))
-    plt.plot(rewards)
+    plt.figure(figsize=(12, 8))
+    plt.plot(rew)
     plt.plot(smoothed_rewards)
     plt.ylabel('Total Rewards')
     plt.xlabel('Episodes')
